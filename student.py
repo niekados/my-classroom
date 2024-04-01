e69043ccb66928d10698cc193a8f2edc3b643b7e
niekados <tiny.post@yahoo.com>
2024-04-01 16:40:57 +0000

    Add Student class to student.py

--- student.py
+++ student.py
@@ -15,3 +15,34 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 # Opening the Google Sheets document named 'my_classroom'
 SHEET = GSPREAD_CLIENT.open('my_classroom')
+
+class Student:
+    """
+    Represents a student in MyClassroom.
+    """
+
+    sid = SHEET.worksheet("sid")  # Get the sid worksheet containing the student ID to iterate over
+
+    def __init__(self, name: str, allergies="", dietary="", medication="", special_needs="", notes=""):
+        """
+        Initialize a new student.
+
+        Args:
+            name (str): The name of the student.
+            allergies (str, optional): Any allergies the student has. Defaults value "".
+            dietary (str, optional): Dietary restrictions of the student. Defaults value "".
+            medication (str, optional): Medications taken by the student. Defaults value "".
+            special_needs (str, optional): Special needs of the student. Defaults value "".
+            notes (str, optional): Additional notes about the student. Defaults value "".
+        """
+        self.name = name
+        self.allergies = allergies
+        self.dietary = dietary
+        self.medication = medication
+        self.special_needs = special_needs
+        self.notes = notes
+        
+        # Assign student id to worksheets A1 cell value
+        self.id = int(Student.sid.acell("A1").value)
+        # Increment ID value by one for each new student
+        Student.sid.update_acell("A1", f"{int(Student.sid.acell("A1").value) + 1}")
