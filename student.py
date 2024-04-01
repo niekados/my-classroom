fb931d3f75eb857ef09ffe7f9ca9a1421b693c38
niekados <tiny.post@yahoo.com>
2024-04-01 16:39:40 +0000

    Create student.py file and add google API authentication to it

---
+++ student.py
@@ -0,0 +1,17 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Google Sheets authentication
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Loading credentials from the JSON file
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+# Opening the Google Sheets document named 'my_classroom'
+SHEET = GSPREAD_CLIENT.open('my_classroom')
