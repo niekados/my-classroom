71f3a71ca9ea9a7e30025cdbdca426eb1b240242
niekados <tiny.post@yahoo.com>
2024-04-04 09:44:50 +0000

    Refactor student.py Python code to comply with PEP8 style guide

--- TESTING.md
+++ TESTING.md
@@ -1,5 +1,7 @@
 # Testing
 
+This project was built using [Code Institute Project Template](https://github.com/Code-Institute-Org/p3-template). HTML and JavaScript were provided with it. Therefore, no HTML, JavaScript, or browser compatibility tests were conducted.
+
 ## PEP8 Validation
 
 ## Manual Testing
@@ -9,46 +11,46 @@
 - **As a teacher, I want easy access to a classroom overview with all students listed.**
   - The Classroom submenu has been implemented and is accessible from the main menu.
 
-  <img src="assets/images/features/classroom-menu.png" alt="classroom menu" style="width:40%;">
+    <img src="assets/images/features/classroom-menu.png" alt="classroom menu" style="width:50%;">
 
   - You can select a single classroom from the menu, and it will be displayed on the screen.
 
-  <img src="assets/images/features/select-classroom.png" alt="select classroom" style="width:40%;">
+    <img src="assets/images/features/select-classroom.png" alt="select classroom" style="width:50%;">
 
 - **As a teacher, I want the ability to filter children who may require additional assistance in the class.**
   - The Classroom submenu offers a menu option to display the classroom table with filtering options for children who need extra attention during classes.
 
-  <img src="assets/images/features/classroom-menu.png" alt="classroom menu" style="width:40%;">
+    <img src="assets/images/features/classroom-menu.png" alt="classroom menu" style="width:50%;">
 
 - **As a kitchen worker, I need access to any additional dietary requirements before preparing meals for the school.**
   - The Kitchen submenu is available, filtering all children with any dietary requirements and allergies, and displays a table for the entire school.
 
-  <img src="assets/images/features/kitchen-menu.png" alt="kitchen menu" style="width:40%;">
+   <img src="assets/images/features/kitchen-menu.png" alt="kitchen menu" style="width:50%;">
 
 - **As a teacher or medical personnel, I want to be able to filter all children in school based on specific criteria such as allergies, dietary requirements, medications, or special needs.**
   - The Classroom submenu provides filtering options for each medical section, including allergies, dietary requirements, medications, special needs, and medical notes. It also shows a full table with all these requirements for the whole school.
 
-  <img src="assets/images/features/medical-menu.png" alt="medical menu" style="width:40%;">
+    <img src="assets/images/features/medical-menu.png" alt="medical menu" style="width:50%;">
 
 - **As a teacher, I want to add a new student to the classroom.**
   - The Admin submenu includes an option to add a new student with a step-by-step interface guiding the teacher through entering each entry.
 
-  <img src="assets/images/features/add-new-student.png" alt="add student" style="width:40%;">
+    <img src="assets/images/features/add-new-student.png" alt="add student" style="width:50%;">
 
 - **As a teacher, I want to create a new classroom.**
   - The Admin submenu offers an option to add a new classroom, validating if a classroom with that name already exists, thus preventing duplicate classroom creations.
 
-  <img src="assets/images/features/add-classroom.png" alt="add classroom" style="width:40%;">
+    <img src="assets/images/features/add-classroom.png" alt="add classroom" style="width:50%;">
 
 - **As a teacher, I want to remove a student from the class.**
   - The Admin submenu provides an option to search for a student, returning a list of students (or multiple students if there are duplicates) and allowing the teacher to choose which student to remove.
 
-  <img src="assets/images/features/remove-student.png" alt="remove student" style="width:40%;">
+    <img src="assets/images/features/remove-student.png" alt="remove student" style="width:50%;">
 
 - **As a teacher, I want to remove a classroom.**
   - The Admin submenu for removing classrooms lists all classrooms already created and offers the option to select which classroom the teacher would like to remove.
 
-  <img src="assets/images/features/remove-classroom.png" alt="remove classroom" style="width:40%;">
+    <img src="assets/images/features/remove-classroom.png" alt="remove classroom" style="width:50%;">
 
 ## Bugs
 
--- student.py
+++ student.py
@@ -1,44 +1,59 @@
 """
-This module provides functionality for managing student information in MyClassroom.
+This module provides functionality for managing
+student information in MyClassroom.
 """
 
-import gspread
-from google.oauth2.service_account import Credentials
+# Third-party imports
+import gspread  # Google Sheets API library
+from google.oauth2.service_account import Credentials  # Google credentials
 
 # Google Sheets authentication
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/drive",
 ]
 
 # Loading credentials from the JSON file
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 # Opening the Google Sheets document named 'my_classroom'
-SHEET = GSPREAD_CLIENT.open('my_classroom')
+SHEET = GSPREAD_CLIENT.open("my_classroom")
 
 
 class Student:
-    """
-    Represents a student in MyClassroom.
-    """
+    """Represents a student in MyClassroom."""
 
-    sid = SHEET.worksheet("sid")  # Get the sid worksheet containing the student ID to iterate over
+    sid = SHEET.worksheet(
+        "sid"
+    )  # Get the sid worksheet containing the student ID to iterate over
 
-    def __init__(self, name: str, allergies="", dietary="", medication="", special_needs="", notes=""):
+    def __init__(
+        self,
+        name: str,
+        allergies="",
+        dietary="",
+        medication="",
+        special_needs="",
+        notes="",
+    ):
         """
         Initialize a new student.
 
         Args:
             name (str): The name of the student.
-            allergies (str, optional): Any allergies the student has. Defaults value "".
-            dietary (str, optional): Dietary restrictions of the student. Defaults value "".
-            medication (str, optional): Medications taken by the student. Defaults value "".
-            special_needs (str, optional): Special needs of the student. Defaults value "".
-            notes (str, optional): Additional notes about the student. Defaults value "".
+            allergies (str, optional): Any allergies the student has.
+                Defaults value "".
+            dietary (str, optional): Dietary restrictions of the student.
+                Defaults value "".
+            medication (str, optional): Medications taken by the student.
+                Defaults value "".
+            special_needs (str, optional): Special needs of the student.
+                Defaults value "".
+            notes (str, optional): Additional notes about the student.
+                Defaults value "".
         """
         self.name = name
         self.allergies = allergies
@@ -46,12 +61,12 @@ class Student:
         self.medication = medication
         self.special_needs = special_needs
         self.notes = notes
-        
+
         # Assign student id to worksheets A1 cell value
         self.id = int(Student.sid.acell("A1").value)
         # Increment ID value by one for each new student
-        Student.sid.update_acell("A1", f"{int(Student.sid.acell('A1').value) + 1}")
-                           
+        cell_value = int(Student.sid.acell('A1').value)
+        Student.sid.update_acell("A1", f"{cell_value + 1}")
 
     def add_student(self, worksheet_name: str):
         """
@@ -63,12 +78,16 @@ class Student:
         class_name = SHEET.worksheet(worksheet_name.upper())
         worksheet_len = len(class_name.col_values(2)) + 1
         # Update cells with student information
-        class_name.update_cell(worksheet_len, 1, f"{int(Student.sid.acell('A1').value) + 1}")
+        class_name.update_cell(
+            worksheet_len, 1, f"{int(Student.sid.acell('A1').value) + 1}"
+        )
         class_name.update_cell(worksheet_len, 2, self.name.title())
         class_name.update_cell(worksheet_len, 3, self.allergies.capitalize())
         class_name.update_cell(worksheet_len, 4, self.dietary.capitalize())
         class_name.update_cell(worksheet_len, 5, self.medication.capitalize())
-        class_name.update_cell(worksheet_len, 6, self.special_needs.capitalize())
+        class_name.update_cell(
+            worksheet_len, 6, self.special_needs.capitalize()
+        )
         class_name.update_cell(worksheet_len, 7, self.notes.capitalize())
 
     @staticmethod
@@ -84,5 +103,8 @@ class Student:
         try:
             worksheet.delete_rows(row_id)
         except Exception as e:
-            print(f"Error occurred while deleting row {row_id} in worksheet {classroom_name.upper()}: {e}")
-            pass  
+            print(
+                f"Error occurred while deleting row {row_id}"
+                f" in worksheet {classroom_name.upper()}: {e}"
+            )
+            pass
